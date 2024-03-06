9317c6136f61f367a7b6a0724e555b7b6180c005
Ashketchup143 <anmlim@addu.edu.ph>
2024-03-06 19:20:32 +0800

    asdasd

---
+++ newindex.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+const port= 3000;
+
+app.use((req, res, next)=>{
+    console.log('Request Made')
+    console.log(`Host: ${req.hostname}`)
+    console.log(`Path: ${req.path}`)
+    console.log(`Method: ${req.method}`)
+    next()
+})
+
+app.get('/', function (req, res) {
+  res.sendFile('./views/home.html', { root: __dirname})
+})
+
+app.get('/about', function (req, res) {
+    res.sendFile('./views/about.html', { root: __dirname})
+})
+
+app.get('/portfolio', function (req, res) {
+    res.sendFile('./views/portfolio.html', { root: __dirname})
+})
+
+app.get('/uniquepage', function (req, res) {
+    res.sendFile('./views/uniquepage.html', { root: __dirname})
+})
+
+app.get('/home',(req,res)=>{
+    res.redirect('/')
+})
+
+app.get('/aboutus',(req,res)=>{
+    res.redirect('/about')
+})
+
+app.use((req,res)=>{
+    res.status(404).sendFile('./views/error.html', { root: __dirname})
+})
+app.listen(port, ()=>{
+    console.log(`Server  is running on port ${port}`)
+});
