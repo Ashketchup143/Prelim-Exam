42b2e998889b0392ca81e50fd23f930d9e23c124
Ashketchup143 <anmlim@addu.edu.ph>
2024-03-25 18:48:32 +0800

    new updates

--- newindex.js
+++ newindex.js
@@ -3,6 +3,10 @@ const app = express()
 app.set('view engine', 'ejs')
 const port= 3000;
 
+const{ students, hello }= require('./mymodule')
+
+console.log(students)
+
 app.use((req, res, next)=>{
     console.log('Request Made')
     console.log(`Host: ${req.hostname}`)
@@ -26,7 +30,7 @@ let e="Desire: Wishes to connect with others and foster friendships."
 let f="Meet Ashley Noel M. Lim, a vibrant student currently pursuing a Bachelor of Science in Information Systems at Ateneo de Davao University. With a passion for both exploring the diverse world of cuisine and nurturing his spiritual connection with God, Ashley embodies a blend of culinary curiosity and profound faith. His infectious happiness and unwavering positivity illuminate his journey, as he eagerly embraces each day with a heart full of gratitude and a desire to connect with others. With his warm demeanor and welcoming spirit, Ashley invites you to join his in the delightful journey of friendship and shared experiences."
 let img="https://scontent.fmnl17-2.fna.fbcdn.net/v/t39.30808-6/240525583_4593098317399941_4698934283417746461_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFk9hTMX1wRcGIirHogCefugdfmN12jm3qB1-Y3XaObeuNw-OaNXFulgyCc21irRzuEFUE1PffMKMe-A37WpAzg&_nc_ohc=ESEjJylmb14AX8_Fq5w&_nc_ht=scontent.fmnl17-2.fna&oh=00_AfCt3BKQiu3nOHlKaBdvLinsSp7030IvdxKdz9B84wIC5w&oe=65FDCF8A"
 app.get('/about', function (req, res) {
-    res.render('about',{a:a,b:b,c:c,d:d,e:e,f:f,img:img} );
+    res.render('about',{a:a,b:b,c:c,d:d,e:e,f:f,img:img, studentData:students, helloFunction:hello} );
     // res.sendFile('./views/about.html', { root: __dirname})
 })
 
